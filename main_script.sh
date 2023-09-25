99108d94df16f79d0e73f360624dc3d7ffc377c0
Carolina Ferreira <42354106+cfatuesta@users.noreply.github.com>
2023-09-25 12:18:52 -0400

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -303,13 +303,3 @@ else
     echo "Exiting."
     exit 1
 fi
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-R9MNVZNGV6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-R9MNVZNGV6');
-</script>
